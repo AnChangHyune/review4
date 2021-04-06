83b1a5414494af692e61e6fbd47e73a0e5fe82a2
AnChangHyune <ymy6534@gmail.com>
2021-04-06 20:21:25 +0900

    메뉴바 추가

--- src/main/resources/static/resource/common.js
+++ src/main/resources/static/resource/common.js
@@ -1,8 +1,26 @@
 function isTouchDevice() {
-  return (('ontouchstart' in window) ||
-          (navigator.maxTouchPoints > 0) ||
-          (navigator.msMaxTouchPoints > 0));
+	return (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
 }
 
 const $html = document.querySelector('html');
-$html.classList.add(isTouchDevice() ? 'touch-posible' : 'touch-imposible'); 
+$html.classList.add(isTouchDevice() ? 'touch-posible' : 'touch-imposible');
+
+function openNav() {
+	document.getElementById("mySidenav").style.display = "block";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.display = "none";
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "0px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
